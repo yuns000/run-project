f702e378e8d698dcbe6127fe040d7cdcf3606fd4
BHW <kassadin34@gmail.com>
2024-03-22 20:49:16 +0900

    비밀번호 해시값으로 변경 후 저장 db accounts_users 비밀번호 길이 증가 계정 생성 json 형식으로 리턴하게 수정 회원가입 구현 메인 페이지 접속 시 로그인 페이지로 이동하게 백에서 구현해둠 login.html base.html 상속받게 수정 완

--- run/accounts/views.py
+++ run/accounts/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
+
+from .models import User
 
 
 def login(request) -> HttpResponse:
     return render(request, 'accounts/login/login.html')
 
 def account_creation(request) -> HttpResponse:
-    return render(request, 'accounts/account_creation/account_creation.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        username = request.POST.get('username')
+        location = request.POST.get('location')
+        
+        if User.objects.filter(email=email).exists():
+            message = 'Email already exists'
+            success = False
+        else:
+            User.create_user(email, password, username, location)
+            message = 'success'
+            success = True
+        return JsonResponse({'success': success, 'message' : message})
+    elif request.method == 'GET':
+        return render(request, 'accounts/account_creation/account_creation.html')
