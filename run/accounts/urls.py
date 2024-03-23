832d164563b4d2f2bc3eac58c85690d429111af0
BHW <kassadin34@gmail.com>
2024-03-24 01:59:55 +0900

    swagger 탭 수정을 위한 코드 변경 필요없는 코드 제거

--- run/accounts/urls.py
+++ run/accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
--- run/run/urls.py
+++ run/run/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from . import views
 
-schema_view = views.schema_view(
-    title='Users API',
-    description='API for users',
-    version='1.0'
+schema_view = get_schema_view(
+   openapi.Info(
+      title="[코딩테스트 문제 생성기(가제)] API",
+      default_version='0.1',
+      description="API for dev",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('api_docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
